8ae235764dd19da2b4737051f44d13c8b7339898
orzzzl <zhuangzeleng19920731@gmail.com>
2015-11-30 17:50:11 -0500

    EMPTY

---
+++ Site.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by zelengzhuang on 11/30/15.
+ */
+public class Site {
+    public ArrayList<Variable> rec;
+    public boolean isGood;
+
+    public Site() {
+        rec.clear();
+        isGood = true;
+    }
+}
